14b0c0405e633332bfca77bb56325b01ac0d8ccd
Perviz Necefov <nadjafovp@gmail.com>
2025-07-03 22:13:42 +0400

    tapsiriq13

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,42 @@ public class Main {
 
     }
 
+    public static void tapsiriq13(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                if (j == 1 || i == j || i == 5){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    public static void tapsiriq12(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                if (j == 1 || j == 5 || i == j){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void tapsiriq11(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                if (i == j || i + j == 6){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
     public static void tapsiriq10(){
         for (int i=1;i<=5;i++){
             for (int j=1;j<=5 -i;j++){
