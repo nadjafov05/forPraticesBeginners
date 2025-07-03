af76c83edbe194cbb83c2f428fc6422d5e5013dc
Perviz Necefov <nadjafovp@gmail.com>
2025-07-03 21:37:39 +0400

    tapsiriq10

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,82 @@ public class Main {
 
     }
 
+    public static void tapsiriq10(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5 -i;j++){
+                System.out.print(" ");
+            }
+            for (int k =1; k<=2 * i -1;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for (int i=4;i>=1;i--){
+            for (int j=1;j<=5 - i;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=2 * i - 1;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void tapsiriq9(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5-i;j++){
+
+                System.out.print(" ");
+            }
+            for (int k = 1;k<=2 * i - 1;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void tapsiriq8(){
+        for (int i=1;i<=5;i++){
+            for (int j = 1; j <= i - 1; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 6 - i; k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void tapsiriq7(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                if (i == 1 || i == 5 || j == 1 || j == 5){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void tapsiriq6(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void tapsiriqi5(){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+
+            for (int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void tapsiriq4(){
         for (int i=1;i<=5;i++){
             for (int j=5;j>=i;j--){
