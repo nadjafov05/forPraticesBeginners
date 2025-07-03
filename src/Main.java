3a502b942aa94fe1b9785548c3382bc3c896d7d7
Perviz Necefov <nadjafovp@gmail.com>
2025-07-03 15:06:35 +0400

    tapsiriq1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("salam");
+        tapsiriq1();
+    }
+
+    public static void tapsiriq1(){
+        //Tapşırıq 1:
+        //Konsolda bir sətrdə 5 ədəd * çap et:
+
+        for (int i=1;i<=5;i++){
+            System.out.print("*");
+        }
     }
 }
